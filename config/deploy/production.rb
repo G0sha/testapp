85475e22f378e5f689dc7b1619125184ca265290
Igor <gg0sha@ya.ru>
2015-08-14 00:57:48 +0300

    Added settings to deploy app

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,9 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-
+role :app, %w{deploy@62.76.191.134}
+role :web, %w{deploy@62.76.191.134}
+role :db,  %w{deploy@62.76.191.134}
 
 # role-based syntax
 # ==================
